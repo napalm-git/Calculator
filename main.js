20a39c67a1ac6079ca33e3e0212663acddce583c
napalm-git <frozenrox22@gmail.com>
2022-07-27 23:09:37 +0300

    Access DOM elements

--- main.js
+++ main.js
@@ -16,3 +16,13 @@ class Calculator {
 
   result() {}
 }
+
+const input = document.querySelector("#history");
+const output = document.querySelector("#result");
+
+const clearBtn = document.querySelector("[data-clear]");
+const backSpcBtn = document.querySelector("[data-backspace]");
+const percentBtn = document.querySelector("[data-percent]");
+const opBtn = document.querySelector("[data-operator]");
+const numBtn = document.querySelector("[data-number]");
+const equalsBtn = document.querySelector("[data-equals]");
